74506fea61ef34fd07a0c968dd6ca10f321cc7e3
joris <jorismartin3@gmail.com>
2025-02-05 11:33:33 +0100

    Initial commit

---
+++ server.php
@@ -0,0 +1,73 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use React\EventLoop\Factory;
+use React\Socket\SecureServer;
+use React\Socket\TcpServer;
+
+$env = parse_ini_file(".env");
+$loop = Factory::create();
+$port = '8000';
+$tcp = new TcpServer('127.0.0.1:'.$port, $loop);
+
+$secureTcp = new SecureServer($tcp, $loop, [
+    'local_cert' => $env["SSL_CERT"],
+    'local_pk' => $env["SSL_KEY"],
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => false
+]);
+
+$logFile = '/var/log/websocket/unbounddream.log';
+
+function logMessage($message) {
+    global $logFile;
+    $timestamp = date('Y-m-d H:i:s');
+    $logMessage = "[$timestamp] | " . $message . "\n";
+    file_put_contents($logFile, $logMessage, FILE_APPEND);
+}
+
+class ServerImpl implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        $conn->send("Hello");
+        logMessage("New connection! ({$conn->resourceId})", "0");
+    }
+
+    public function onMessage(ConnectionInterface $conn, $raw) {
+        logMessage(sprintf("New message from '%s': %s", $conn->resourceId, $raw));
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        logMessage("Connection {$conn->resourceId} is gone");
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        logMessage("An error occured on connection {$conn->resourceId}: {$e->getMessage()}", "0");
+        $conn->close();
+    }
+}
+
+$server = new IoServer(
+    new HttpServer(
+        new WsServer(
+            new ServerImpl()
+        )
+    ),
+    $secureTcp,
+    $loop
+);
+echo "Server created on port $port\n\n";
+$server->run();
